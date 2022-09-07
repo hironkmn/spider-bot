610b394b8230c0b65f30766d24b96a469a31bf41
mzerrouk <meidine59@gmail.com>
2022-09-07 15:48:52 +0200

    fix import

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from random import randint
 import discord
 from discord.ext import commands
 from discord import app_commands
-import interaction
+import interactions
 import os
 
 config = os.getenv("TOKEN")
