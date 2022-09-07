bca6b6bd7705dff8a8284bd841d0208ef1f60c33
mzerrouk <meidine59@gmail.com>
2022-09-07 16:53:09 +0200

    Correction

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ from random import randint
 import discord
 from discord.ext import commands
 from discord import app_commands
+import interactions
 import os
+import asyncio
 
 config = os.getenv("TOKEN")
 
@@ -11,11 +13,7 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='s!', intents=intents)
-
-async def setup_hook(self):
-        for ext in self.initial_extensions:
-            await self.load_extension(ext)
-        await bot.tree.sync(guild = discord.Object(id="1001141800448303195"))
+client = interactions.Client(config)
 
 @bot.event
 async def on_ready():
@@ -26,25 +24,25 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-@bot.hybrid_command()
+@bot.command()
 async def rire(ctx):
     desc="<@" + str(ctx.author.id) + "> est mort de rire."
     embed=discord.Embed(description=desc, color=0xFF5733)
     embed.set_image(url='https://c.tenor.com/wIxFiobxxbIAAAAC/john-jonah-jameson-lol.gif')
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def aide(ctx):
     await ctx.send("Salut ! C'est Spider-Bot ! L'araignée sympa du serveur !\nJe vois que tu galères concernant mon utilisation, voici donc l'aide <:spiderfight:1003075153934159893> !\n\n`s!rire`: te permet de montrer tes belles dents lorsque tu es en train de rire !\n`s!photo`: te permet de prendre ton/ta crush en photo. Tu te débrouilles avec la justice par la suite...\n`s!kiss`: te permet d'embrasser l'amour de ta vie... ou t'es juste un grand malade mental.\n`s!bully`: te permet de bully la personne de ton choix... Cela me rappelle mes années de fac avec Flash Thompson...\n`s!dance`: permet de montrer à tout le serveur tes talents de danseur! Je préfère qu'on évite de parler de cette scène...\n`s!cry`: te permet de pleurer. C'est les émotions comme dirait un grand footballeur...\n`s!death`: te permet de mourir. Adieu monde cruel...\n`s!wtf`: te permet de montrer ton incompréhension. On est d'jeuns donc on dit ouate ze feuk!\n`s!hug`: te permet de faire un calîn à la personne de ton choix. J'espère juste que tu tiens pas une pancarte free hugs à la Japan Expo...")
 
-@bot.hybrid_command()
+@bot.command()
 async def photo(ctx, utilisateur: discord.Member):
     desc="<@" + str(ctx.author.id) +"> prends en photo " + utilisateur.mention
     embed = discord.Embed(description=desc, color=0xFF5733)
     embed.set_image(url="https://c.tenor.com/aw6SxQnpLvAAAAAd/spider-man-taking-pictures.gif")
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def kiss(ctx, utilisateur: discord.Member):
     kisses = ["https://c.tenor.com/mVhow8EMB94AAAAC/emma-andrew.gif", "https://c.tenor.com/EPo3vP8MMD0AAAAC/spiderman-mary-jane.gif"]
     aleatoire = randint(0,len(kisses)-1)
@@ -54,7 +52,7 @@ async def kiss(ctx, utilisateur: discord.Member):
     embed.set_image(url=truekiss)
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def bully(ctx, utilisateur: discord.Member):
     bullies = ["https://c.tenor.com/LslUNhtr0bAAAAAC/tobey-maguire-gonna-cry.gif","https://c.tenor.com/NeSd-6B6wl4AAAAC/tu-vas-chialer-spiderman.gif"]
     aleatoire = randint(0,len(bullies)-1)
@@ -63,35 +61,35 @@ async def bully(ctx, utilisateur: discord.Member):
     embed.set_image(url=bullies[aleatoire])
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def dance(ctx):
   desc = "<@" + str(ctx.author.id) + "> danse comme un roi !!"
   embed = discord.Embed(description=desc, color=0xFF5733)
   embed.set_image(url="https://c.tenor.com/9qZhM0uswAYAAAAd/bully-maguire-dance.gif")
   await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def cry(ctx):
   desc = "<@" + str(ctx.author.id) + "> chiale ses grands morts."
   embed = discord.Embed(description=desc, color=0xFF5733)
   embed.set_image(url="https://ic.pics.livejournal.com/airockz69/15946583/46385/46385_900.gif")
   await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def death(ctx):
     desc = "<@" + str(ctx.author.id) + "> se retrouve à l'état de poussières..."
     embed = discord.Embed(description=desc, color=0xFF5733)
     embed.set_image(url="https://c.tenor.com/-gArcqUHlNgAAAAd/spiderman-death.gif")
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def wtf(ctx):
     desc = "<@"+ str(ctx.author.id) + "> se dit quoi de la fuck ????"
     embed= discord.Embed(description=desc, color=0xFF5733)
     embed.set_image(url="https://c.tenor.com/4JVPWD8GrXkAAAAC/spider-man-shocked.gif")
     await ctx.send(embed=embed)
 
-@bot.hybrid_command()
+@bot.command()
 async def hug(ctx, utilisateur: discord.Member):
     hugs = ["https://c.tenor.com/rW4HtdpmZxAAAAAd/hug-spider-man.gif","https://c.tenor.com/gG3tX97uQaQAAAAC/spiderman-iron-man.gif", "https://c.tenor.com/CtTwr740BEsAAAAC/hug-spiderman.gif"]
     aleatoire = randint(0,len(hugs)-1)
@@ -100,10 +98,28 @@ async def hug(ctx, utilisateur: discord.Member):
     embed.set_image(url=hugs[aleatoire])
     await ctx.send(embed=embed)
 
+@client.command(
+    name="death",
+    description="Te permet de mourir. Adieu monde cruel..."
+)
+async def death2(ctx):
+    desc = "<@" + str(ctx.author.id) + "> se retrouve à l'état de poussières..."
+    embed = discord.Embed(description=desc, color=0xFF5733)
+    embed.set_image(url="https://c.tenor.com/-gArcqUHlNgAAAAd/spiderman-death.gif")
+    await ctx.send(embed=embed)
+
 @bot.event
 async def on_message(message):
     # do some extra stuff here
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Spider-Man: No Way Home"))
     await bot.process_commands(message)
 
-bot.run(config)
+loop = asyncio.get_event_loop()
+
+task2 = loop.create_task(bot.start(config))
+task1 = loop.create_task(client._ready())
+gathered = asyncio.gather(task1, task2)
+loop.run_until_complete(gathered)
+
+
+##bot.run(config)
